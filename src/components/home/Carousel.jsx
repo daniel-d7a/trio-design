f32014ee36f9e3fc23b6dab13f86976f03f2c59c
daniel-d7a <eyad.taym.saba@gmail.com>
2023-03-24 23:25:06 +0200

    added carousel controls

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
+import arrow from "../../assets/left.svg";
 
 export default function Carousel() {
-
   const sofa1 =
     "https://images.sofology.co.uk/q_70,fl_lossy,f_auto/v1673455389/shop-fabric_ho4yn7.jpg";
   const sofa2 =
@@ -19,45 +19,43 @@ export default function Carousel() {
   const imgs = [chair1, chair2, chair3];
   const bigImgs = [sofa1, sofa2, sofa3];
 
-  function scrollFade(i) {
-    const carousel = document.getElementById("carousel");
-      const images = carousel?.querySelectorAll("img");
-      images[i].style.opacity = 100;
-      images[i].style.zIndex = 10;
+  const [index, setIndex] = useState(0);
+  const [isClicking, setIsClicking] = useState(false);
 
-      const beforeScrollIndex = (i - 1 + 3) % 3;
+  const clickFunction = clickDebouncer();
 
-      images[beforeScrollIndex].style.opacity = 0;
-      images[beforeScrollIndex].style.zIndex = 0;
-  }
+  let timer;
 
-  useEffect(() => {
-    const carousel = document.getElementById("carousel");
-    const images = carousel?.querySelectorAll("img");
-    images[0].style.opacity = 100;
-    images[0].style.zIndex = 10;
-
-    let i = 1;
-    const carouselInterval = setInterval(() => {
-      scrollFade(i);
-      i = (i + 1) % 3;
-    }, 3500);
-
-    return () => {
-      clearInterval(carouselInterval);
-    }
-
-  }, []);
+  useClickEffect();
+  useCarousel();
 
   return (
     <>
       <section>
-        <div className="w-[80%] m-auto h-full mb-10 flex items-center justify-center">
+        <div className="w-[80%] m-auto h-full mb-4 flex items-center justify-center">
           <div id="carousel" className="relative w-full h-[80vh] md:h-96 ">
+            <img
+              onClick={() => {
+                setIsClicking(true);
+                setIndex((index - 1 + 3) % 3);
+                scrollFade("backward");
+              }}
+              src={arrow}
+              className="w-10 cursor-pointer absolute z-50 top-1/2 -left-10 -translate-y-1/2"
+            />
+            <img
+              onClick={() => {
+                setIsClicking(true);
+                setIndex((index + 1) % 3);
+                scrollFade();
+              }}
+              src={arrow}
+              className="w-10 rotate-180 cursor-pointer absolute z-50 top-1/2 -right-10 -translate-y-1/2"
+            />
             {imgs.map((_, i) => (
               <div
                 key={`asdf${i}`}
-                className="w-full h-full top-0 left-0 m-auto absolute"
+                className="carousel-div w-full h-full top-0 left-0 m-auto absolute"
               >
                 <picture>
                   <source
@@ -77,4 +75,73 @@ export default function Carousel() {
       </section>
     </>
   );
+
+  function clickDebouncer() {
+    return function () {
+      clearTimeout(timer);
+      // console.log("timer debounced");
+      timer = setTimeout(() => {
+        setIsClicking(false);
+      }, 1500);
+    };
+  }
+
+  function scrollFade(direction = "forward") {
+    console.log(index);
+
+    const carousel = document.getElementById("carousel");
+    const images = carousel?.querySelectorAll(".carousel-div img");
+    const beforeScrollIndex = (index - 1 + 3) % 3;
+
+    if (direction === "forward") {
+      images[index].style.opacity = 100;
+      images[index].style.zIndex = 10;
+
+      images[beforeScrollIndex].style.opacity = 0;
+      images[beforeScrollIndex].style.zIndex = 0;
+    } else {
+      images[beforeScrollIndex].style.opacity = 100;
+      images[beforeScrollIndex].style.zIndex = 10;
+
+      images[index].style.opacity = 0;
+      images[index].style.zIndex = 0;
+    }
+  }
+
+  function useClickEffect() {
+    useEffect(() => {
+      if (isClicking) {
+        clickFunction();
+      }
+      return () => {
+        clearTimeout(timer);
+      };
+    });
+  }
+
+  function useCarousel() {
+    useEffect(() => {
+      const carousel = document.getElementById("carousel");
+      const images = carousel?.querySelectorAll(".carousel-div img");
+      images[0].style.opacity = 100;
+      images[0].style.zIndex = 10;
+    }, []);
+
+    useEffect(() => {
+      let interval;
+
+      // console.log("is clicking: ", isClicking);
+      if (isClicking) {
+        clearInterval(interval);
+      } else {
+        interval = setInterval(() => {
+          setIndex((index + 1) % 3);
+          scrollFade();
+        }, 3500);
+      }
+      return () => {
+        clearInterval(interval);
+      };
+    }, [index, isClicking]);
+  }
 }
